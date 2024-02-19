ef2c3b321a3f83dad786913415eb3867cd8f5567
MarcosAugusto10 <48873276+MarcosAugusto10@users.noreply.github.com>
2024-02-19 19:12:58 -0300

    ajuste formatação

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,27 +1,26 @@
 //adicionar alguma interação com outros components e evento
 
 <template>
-<footer class="footer">
-  <div class="column is-pulled-right">
-    <p>
-      <strong >Teste de component footer</strong>  
-    </p>
-    
-  </div>
-  <label class="texto">Texto label</label>
-</footer>
+  <footer class="footer">
+    <div class="column is-pulled-right">
+      <p>
+        <strong>Teste de component footer</strong>
+      </p>
+
+    </div>
+    <label class="texto">Texto label</label>
+  </footer>
 </template>
 <script lang="ts">
-import {  defineComponent  } from 'vue';
+import { defineComponent } from 'vue';
 export default defineComponent(
-{
-    name:"FooterComponent"
-})
+  {
+    name: "FooterComponent"
+  })
 </script>
 <style>
-.texto
-{
-font-size: 50px;
-float: left;
+.texto {
+  font-size: 50px;
+  float: left;
 }
 </style>
